f028edb15ec478460a44f87708ba8a28783813c8
alex <alex.zdns@gmail.com>
2023-12-16 09:50:18 +0100

    Migrate to gradle 8.2

--- app/src/main/java/ru/alexzdns/movieapp/ui/movie/details/MovieDetailsScreen.kt
+++ app/src/main/java/ru/alexzdns/movieapp/ui/movie/details/MovieDetailsScreen.kt
@@ -1,8 +1,13 @@
 package ru.alexzdns.movieapp.ui.movie.details
 
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.input.nestedscroll.nestedScroll
 import androidx.hilt.navigation.compose.hiltViewModel
 import ru.alexzdns.movieapp.domain.models.LoadableResult
 import ru.alexzdns.movieapp.domain.models.Movie
