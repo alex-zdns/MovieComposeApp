d0a667b70bab4419c4192c4ac2db10358f3f0ad4
alex <alex.zdns@gmail.com>
2023-12-16 11:25:16 +0100

    Remove old code

--- app/src/main/java/ru/alexzdns/movieapp/ui/movie/list/MoviesListViewModel.kt
+++ app/src/main/java/ru/alexzdns/movieapp/ui/movie/list/MoviesListViewModel.kt
@@ -1,20 +1,11 @@
 package ru.alexzdns.movieapp.ui.movie.list
 
-import android.util.Log
 import androidx.lifecycle.ViewModel
-import androidx.lifecycle.viewModelScope
 import androidx.paging.Pager
 import androidx.paging.PagingConfig
 import dagger.hilt.android.lifecycle.HiltViewModel
-import java.lang.Exception
 import javax.inject.Inject
-import kotlinx.coroutines.flow.MutableStateFlow
-import kotlinx.coroutines.flow.StateFlow
-import kotlinx.coroutines.flow.asStateFlow
-import kotlinx.coroutines.launch
 import ru.alexzdns.movieapp.data.paging.MoviePagingSource
-import ru.alexzdns.movieapp.domain.models.LoadableResult
-import ru.alexzdns.movieapp.domain.models.Movie
 import ru.alexzdns.movieapp.domain.repository.MovieRepository
 
 @HiltViewModel
@@ -22,29 +13,7 @@ class MoviesListViewModel @Inject constructor(
     private val repository: MovieRepository,
 ) : ViewModel() {
 
-/*    private val _uiState: MutableStateFlow<LoadableResult<List<Movie>>> =
-        MutableStateFlow(LoadableResult.Loading)
-    val uiState: StateFlow<LoadableResult<List<Movie>>> = _uiState.asStateFlow()*/
-
     val movieFlow = Pager(PagingConfig(20)) {
         MoviePagingSource(repository)
     }.flow
-
-/*    init {
-        loadData()
-    }*/
-
-/*    fun loadData() {
-        viewModelScope.launch {
-            _uiState.value = LoadableResult.Loading
-
-            try {
-                val movies = repository.loadMoviesFromServer()
-                _uiState.value = LoadableResult.Success(movies)
-            } catch (e: Exception) {
-                Log.e(this::class.simpleName, e.toString())
-                _uiState.value = LoadableResult.Failure(e)
-            }
-        }
-    }*/
 }
